e52246f402b9b4ea6c060685c3a1bfdbf482af0c
Oskar Lindblad <lindbladoskar@gmail.com>
2019-10-27 15:55:17 +0100

    First try

---
+++ game.js
@@ -0,0 +1,6 @@
+/** (mjukstart med JSDOC)
+ * @param {string} gameTexts - Berättelsetexten i spelet
+ * @param {string} playerInput - Spelarens gäring i spelet
+ */
+const gameTexts = document.getElementById('gameTexts');
+const playerInput = document.getElementById('playerInput');
